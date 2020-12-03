4ec39adbdfb506b3e629ec9788fa48647943863f
Anthony Belcastro <belcastt@live.com>
2020-12-03 13:27:34 -0700

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,6 @@ const app = express();
 
 app.use(express.json())
 
-app.get("/", (req, res, next) => {
-    res.send("Backend Begins...");
-});
-
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -32,6 +27,15 @@ app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/eco/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'eco', 'build', 'index.html')))
+} else
+    app.get("/", (req, res, next) => {
+        res.send("Backend Begins...");
+    });
 
 app.use(notFound);
 
